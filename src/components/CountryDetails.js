fa454cba9c007acf26ea36b2137ebec6254df29c
Guilherme Candeia <guilherme.candeia@outlook.com>
2021-06-27 22:41:14 -0300

    done

---
+++ src/components/CountryDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import countries from '../countries.json';
+
+class CountryDetails extends React.Component {
+  render() {
+    
+}
+
+export default CountryDetails;
